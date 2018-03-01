28d27058600a5bf34f2305bfe3dffb24a1383f4f
Sullivan Beck <sulbeck@heather.osg.ufl.edu>
2018-03-01 09:09:55 -0500

    Changes

--- lib/Template/Plugin/ListOps.pm
+++ lib/Template/Plugin/ListOps.pm
@@ -1,5 +1,5 @@
 package Template::Plugin::ListOps;
-# Copyright (c) 2007-2014 Sullivan Beck. All rights reserved.
+# Copyright (c) 2007-2015 Sullivan Beck. All rights reserved.
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 
--- t/01.t
+++ t/01.t
@@ -33,6 +33,6 @@ test($test,$data,$runtests);
 # cperl-continued-brace-offset: 0
 # cperl-brace-offset: 0
 # cperl-brace-imaginary-offset: 0
-# cperl-label-offset: -2
+# cperl-label-offset: 0
 # End:
 
